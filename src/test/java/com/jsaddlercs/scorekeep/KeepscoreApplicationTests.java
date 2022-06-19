654b283bb3e353cd9406971aac4194fb7eee3166
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-06-19 07:52:30 -0400

    Test configuration should point to KeepscoreApplication

--- src/test/java/com/jsaddlercs/scorekeep/KeepscoreApplicationTests.java
+++ src/test/java/com/jsaddlercs/scorekeep/KeepscoreApplicationTests.java
@@ -3,7 +3,9 @@ package com.jsaddlercs.scorekeep;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.jsaddlercs.keepscore.KeepscoreApplication;
+
+@SpringBootTest(classes=KeepscoreApplication.class)
 class KeepscoreApplicationTests {
 
 	@Test
