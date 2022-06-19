ccaa1562a99bb15bf2e0c2dc7aad192f7352fc5c
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-06-18 22:28:47 -0400

    Update Database Files

--- src/main/resources/db/migration/V1.2__CreateGameScoreSchema.sql
+++ src/main/resources/db/migration/V1.2__CreateGameScoreSchema.sql
@@ -5,11 +5,12 @@ CREATE TABLE game (
   common_name varchar(40)
 );
 
-CREATE TABLE score(
+CREATE TABLE score( 
   turn_order int, 
   person_nickname varchar(10),
   person_name varchar(80) default '',
   new_score int not null,
   game_id bigint not null,
+  recording_id bigserial PRIMARY KEY,
   foreign key (game_id) references game(id)
 );
