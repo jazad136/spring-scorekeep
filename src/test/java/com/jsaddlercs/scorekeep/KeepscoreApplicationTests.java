113e6e9989c8947d5df54a5c2060f6481566d65d
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-06-21 19:43:55 -0400

    Add one test: personNicknameMatches

--- src/test/java/com/jsaddlercs/scorekeep/KeepscoreApplicationTests.java
+++ src/test/java/com/jsaddlercs/scorekeep/KeepscoreApplicationTests.java
@@ -1,13 +1,29 @@
 package com.jsaddlercs.scorekeep;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.jsaddlercs.keepscore.KeepscoreApplication;
+import com.jsaddlercs.keepscore.access.dao.ScoreDAO;
+import com.jsaddlercs.keepscore.data.model.ScoreSet;
+import com.jsaddlercs.keepscore.data.model.ScoreSetRecording;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes=KeepscoreApplication.class)
 class KeepscoreApplicationTests {
 
+	@Autowired ScoreDAO scoreDao;
+	
+	@Test
+	void personNicknameMatches() { 
+		ScoreSet set = scoreDao.findById("Jon");
+		List<ScoreSetRecording> jonRec = set.getScoreSet().get(1);
+		assertThat(jonRec.get(0).getPersonNickname()).isEqualTo("Jon");
+	}
 	@Test
 	void contextLoads() {
 	}
